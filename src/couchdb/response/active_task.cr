07df208fd83bb807ebcecd0a7a18cea54f12b97f
vectorselector <vectorselector@gmail.com>
2020-11-07 22:07:49 +0000

    Update active_task.cr
    Error: no overload matches 'Int32.to_json' with type JSON::Builder ... IDK...

--- src/couchdb/response/active_task.cr
+++ src/couchdb/response/active_task.cr
@@ -7,7 +7,7 @@ module CouchDB::Response
       "change_done" => Int64,
       "database" => String,
       "pid" => String,
-      "progress" => Int32,
+      "progress" => Int,
       "started_on" => Int64,
       "total_changes" => Int64,
       "type" => String,
