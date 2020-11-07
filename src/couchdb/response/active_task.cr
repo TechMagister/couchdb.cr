737bbea1a118f9b45c663a2756ee053576e661e7
vectorselector <vectorselector@gmail.com>
2020-11-07 21:56:11 +0000

    Update active_task.cr

--- src/couchdb/response/active_task.cr
+++ src/couchdb/response/active_task.cr
@@ -3,16 +3,16 @@ require "json"
 module CouchDB::Response
 
   class ActiveTask
-    JSON.mapping(
-      change_done: Int64,
-      database: String,
-      pid: String,
-      progress: Int32,
-      started_on: Int64,
-      total_changes: Int64,
-      type: String,
-      updated_on: Int64
-    )
+    {
+      "change_done" => Int64,
+      "database" => String,
+      "pid" => String,
+      "progress" => Int32,
+      "started_on" => Int64,
+      "total_changes" => Int64,
+      "type" => String,
+      "updated_on" => Int64
+    }.to_json
   end
 
 end
